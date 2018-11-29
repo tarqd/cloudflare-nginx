ad17130c0eb2e16595a7befec0e01940c28639fc
Christopher Tarquini <ctarquini@linode.com>
2018-11-29 03:40:32 -0500

    add release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -12,6 +12,8 @@ docker container cp extract:/tmp/nginx-pkg /tmp/nginx-pkg
 mv /tmp/nginx-pkg/*.deb ./release
 mv /tmp/nginx-pkg/*.rpm ./release
 mv /tmp/nginx-pkg/cloudflare-ips/* ./cloudflare-ips
+mv /tmp/nginx-pkg/cloudflare.d/*v4.conf ./cloudflare.d/
+mv /tmp/nginx-pkg/cloudflare.d/*v6.conf ./cloudflare.d/
 rm -rf /tmp/nginx-pkg
 docker container rm -f extract
 
